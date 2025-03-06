47cfc2b8c5771358579e3711f666f4417f9308ec
Fitsumhelina <permin1501424@gmail.com>
2025-03-06 11:33:43 +0300

    feat(migration):migartion file updated

--- dayOne/database/migrations/2025_03_06_080209_create_tags_table.php
+++ dayOne/database/migrations/2025_03_06_080209_create_tags_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\Tag;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,14 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Job::class);
+            $table->foreignIdFor(Tag::class);
             $table->timestamps();
         });
     }
