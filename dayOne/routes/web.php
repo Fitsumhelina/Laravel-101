bc9d4f0b0d41da9c4cdb402602092e2919c0dfa3
Fitsumhelina <permin1501424@gmail.com>
2025-03-09 19:29:21 +0300

    feat (api): api grouped in

--- dayOne/routes/web.php
+++ dayOne/routes/web.php
@@ -3,26 +3,15 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-//home
 Route::view('/', 'home');
-//index
-Route::get('/jobs', [JobController::class, 'index'] );
+Route::controller(JobController::class)->group(function(){
+    Route::get('/jobs', 'index');
+    Route::get('/jobs/create', 'create');
+    Route::get('/jobs/{job}', 'show');
+    Route::post('/jobs', 'store');
+    Route::get('/jobs/{job}/edit', 'edit');
+    Route::patch('/jobs/{job}', 'update');
+    Route::delete('/jobs/{job}', 'destroy');
+});
 
-//create
-Route::get(('/jobs/create'), [JobController::class, 'create'] );
-
-//show
-Route::get('/jobs/{job}', [JobController::class, 'show'] );
-//store
-Route::post('/jobs', [JobController::class, 'store'] );
-//edit
-Route::get('/jobs/{job}/edit',[JobController::class, 'edit'] );
-
-//update
-Route::patch('/jobs/{job}',[JobController::class, 'update'] );
-
-//delete
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
-
-//contact
 Route::view('/contact', 'contact');
